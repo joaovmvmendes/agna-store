2adc3f56835f9a08ba36627706054d0294127fbb
Your NameJoaovmvmendes <joao.vmvmendes@gmail.com>
2021-11-22 10:24:48 -0300

    add firebase environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,27 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCDdTeCd8TZgZbyt-PGcppiZvP5xf5xby4",
+  authDomain: "agnastore210421.firebaseapp.com",
+  projectId: "agnastore210421",
+  storageBucket: "agnastore210421.appspot.com",
+  messagingSenderId: "561292237193",
+  appId: "1:561292237193:web:9c792d843982923c23a958",
+  measurementId: "G-TXS38Z30Q1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const environment = {
   production: false
