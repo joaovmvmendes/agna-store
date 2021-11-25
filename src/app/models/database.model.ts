a80bb1d6c49b372a45d8551fd55413f5510a22a7
Your NameJoaovmvmendes <joao.vmvmendes@gmail.com>
2021-11-25 10:48:50 -0300

    create database model

---
+++ src/app/models/database.model.ts
@@ -0,0 +1,15 @@
+
+export interface FirebaseUserProfile {
+  idCadastro: string;
+  displayName: string;
+  email:string;
+  photoURL:string;
+}
+
+export interface Produto {
+  idProd:string;
+  nomeProd:string;
+  descProd:string;
+  valorProd:number;
+  imgProd:string;
+}
